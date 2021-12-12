57e9e7f3eec8165c8f1443719d604ab802e03627
UjenKI <straight7outta7compton@gmail.com>
2021-12-12 22:32:36 +0200

    lesson 51 - users API (axios)

--- src/component/users/Users.jsx
+++ src/component/users/Users.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
 
+import * as axios from 'axios';
+
+import userPhoto from '../../img/Tony.png';
+
 import style from './Users.module.css';
 
 const Users = (props) => {
     console.log(props.users.length)
     if(props.users.length === 0){
-        props.setUsers([
-            {id: 1, imgUrl: 'https://www.pngarts.com/explore/153009',nickName: 'Nikita', followed: false, status: 'IT-samuraj', location: {country: 'USA', city: 'LA'}},
-            {id: 2, imgUrl: '',nickName: 'NickJKL', followed: false, status: 'IT-samuraj', location: {country: 'Ukraine', city: 'Kyiv'}},
-            {id: 3, imgUrl: '',nickName: 'NataliPt', followed: false, status: 'IT-samuraj', location: {country: 'OAE', city: 'Dubai'}},
-            {id: 4, imgUrl: '',nickName: 'TonyStark', followed: false, status: 'IT-samuraj', location: {country: 'USA', city: 'Miami'}}
-        ])
+        // props.setUsers([
+        //     {id: 1, imgUrl: 'https://www.pngarts.com/explore/153009',nickName: 'Nikita', followed: false, status: 'IT-samuraj', location: {country: 'USA', city: 'LA'}},
+        //     {id: 2, imgUrl: '',nickName: 'NickJKL', followed: false, status: 'IT-samuraj', location: {country: 'Ukraine', city: 'Kyiv'}},
+        //     {id: 3, imgUrl: '',nickName: 'NataliPt', followed: false, status: 'IT-samuraj', location: {country: 'OAE', city: 'Dubai'}},
+        //     {id: 4, imgUrl: '',nickName: 'TonyStark', followed: false, status: 'IT-samuraj', location: {country: 'USA', city: 'Miami'}}
+        // ])
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(res => {
+            props.setUsers( res.data.items);
+        })
+
     }
     // console.log(props.users)
     let userList = props.users.map((user) => {
         return (
             <li key={user.id} className={ style.userItem }>
                 <div className={style.photoWrapper}>
-                    <img src={user.imgUrl} alt="img"/>
+                    <img src={ user.photos.small !== null ? user.photos.small : userPhoto} alt="img"/>
                 </div>
                 <div className={ style.userInfo }>
-                    <p>{user.nickName}</p>
+                    <p>{user.name}</p>
                     <p>{user.status}</p>
-                    <p>{user.location.country}</p>
-                    <p>{user.location.city}</p>
+                    {/* <p>{user.location.country}</p>
+                    <p>{user.location.city}</p> */}
                     {user.followed ? <button onClick = { () => {props.unFollow(user.id)} }>Unfollow</button>
                                    : <button onClick = { () => {props.follow(user.id)} }>Follow</button>}
                 </div>
